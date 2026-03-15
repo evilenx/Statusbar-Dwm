1eaeb46f365437b284d255bafa0ed356d1b5110f
evilenx <evilenx@proton.me>
2026-03-14 19:16:57 -0500

    feat(statusbar): integrate volume display into status bar

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod cpu;
 mod stats;
+mod volume;
 mod x11;
 
 use std::sync::{Arc, Mutex};
@@ -7,30 +8,47 @@ use std::thread;
 use chrono::Local;
 use cpu::CpuData;
 use stats::StatsData;
+use volume::VolumeData;
 use x11::X11;
-
 use std::time::Duration;
+
 fn main() {
     let x11 = X11::new().expect("X11 failed");
-    
-    let cpu = Arc::new(Mutex::new(CpuData{usage: "CPU: 0%".into()}));
-    let stats = Arc::new(Mutex::new(StatsData{
+
+    let cpu = Arc::new(Mutex::new(CpuData {
+        usage: "CPU: --%".into(),
+    }));
+    let stats = Arc::new(Mutex::new(StatsData {
         battery: "Bat: --".into(),
-        memory: "RAM: --".into()
+        memory:  "RAM: --".into(),
+    }));
+    let vol = Arc::new(Mutex::new(VolumeData {
+        display: "🔊 --%".into(),
     }));
 
-    thread::spawn({let c=cpu.clone(); move|| cpu::monitor(c)});
-    thread::spawn({let s=stats.clone(); move|| stats::monitor(s)});
+    thread::spawn({ let c = cpu.clone();   move || cpu::monitor(c)    });
+    thread::spawn({ let s = stats.clone(); move || stats::monitor(s)  });
+    thread::spawn({ let v = vol.clone();   move || volume::monitor(v) });
 
     loop {
-        let time = Local::now().format("%d-%m-%Y %T.%3f").to_string();
+        let time  = Local::now().format("%d-%m-%Y %T.%3f").to_string();
         let stats = stats.lock().unwrap();
-        
-        let bar = format!("{time} | {} | {} | {}",
-                         stats.battery, stats.memory, cpu.lock().unwrap().usage);
-        
+        let vol   = vol.lock().unwrap();
+        let cpu   = cpu.lock().unwrap();
+
+        let bar = format!(
+            "{time} | {} | {} | {} | {}",
+            stats.battery,
+            stats.memory,
+            cpu.usage,
+            vol.display,
+        );
+
         x11.set_title(&format!("{bar}\0"));
-				std::thread::sleep(Duration::from_nanos((1e9 / 144.) as u64));
-	
+        drop(stats);
+        drop(vol);
+        drop(cpu);
+
+        std::thread::sleep(Duration::from_nanos((1e9 / 144.) as u64));
     }
 }
